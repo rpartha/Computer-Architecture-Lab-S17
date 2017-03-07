38be3eea5f2fdb84bfb18a0714915509d75fe75b
Ramaseshan Parthasarathy <tilluboy@gmail.com>
2017-03-06 23:59:12 -0500

    made input exceptions

--- Lab2/assignment_2.asm
+++ Lab2/assignment_2.asm
@@ -3,16 +3,23 @@
   .globl main
 
 main:
+  li $v0, 4
+  la $a0, input
+  syscall
   li $v0, 5 #get user input
   syscall
-  move $s0, $v0  #n
-  add $t0, $zero, $s0 #$t0 = $s0
-  li $t1, 0 #found boolean
-  li $t2, 0 #max prime
+  move $t0, $v0  #n
+  bge $t0, 101, badInput
+
+badInput:
+  li $v0, 4
+  la $a0, error
+  syscall
 
 EXIT:
-  move $a0, $t0
-  li $v0, 4
-  syscall
   li $v0, 10
   syscall
+
+  .data
+    input: .asciiz "Enter an integer <= 100: "
+    error: .asciiz "Input was > 100"
