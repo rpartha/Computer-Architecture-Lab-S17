9763626c8a2fe08384046cf3984e5ea4e6406d2c
Ramaseshan Parthasarathy <tilluboy@gmail.com>
2017-03-06 20:24:07 -0500

    ensured user input >= 0

--- Lab2/assignment_1.asm
+++ Lab2/assignment_1.asm
@@ -3,15 +3,21 @@
 	.globl main
 
 main:
-	li $v0, 5 #get user input
-	syscall
-	move $t0, $v0  #n
-	#slt $t1, $t0, $zero #if $t0 < 0
-	#li $v1, 5  #system call code for error message
-	#la $a0, error_msg #load address of error message into $a0
-	#syscall
-	li $t2, 0  #counter = i
-	li $t3, 0 #sum
+
+li $t0, -1 #initialize $t0
+
+getInput: #keep getting input until >= 0
+		bgez $t0, loop #branch >= 0
+		li $v0, 4
+		la $a0, input
+		syscall
+		li $v0, 5 #get user input
+		syscall
+		move $t0, $v0  #n
+		j getInput
+
+li $t2, 0  #counter = i
+li $t3, 0 #sum
 
 loop:
 	add $t3, $t3, $t2  #sum += i
@@ -30,5 +36,6 @@ exit:
 	syscall
 
 	.data
+		input: .asciiz "Enter a non-negative number:  \n"
 		error_msg: .asciiz "User entered negative integer!"
 		total: .asciiz "Sum: "
